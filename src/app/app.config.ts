545c831f0b5034c4a29384a39e3bb4bbacd901e3
Gonçalo Cardeira <goncalo.cardeira@gmail.com>
2024-04-04 15:17:59 +0100

    build(deps): Integrate firestore from firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { provideFirestore, getFirestore } from "@angular/fire/firestore";
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes), 
+    provideAnimationsAsync(), 
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase)), 
+      provideFirestore(() => getFirestore())),
+    ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+    apiKey: "AIzaSyA_xCX9g3B8LUilPIme1GTRLucDKQYfOyg",
+    authDomain: "portfolio-89c9c.firebaseapp.com",
+    projectId: "portfolio-89c9c",
+    storageBucket: "portfolio-89c9c.appspot.com",
+    messagingSenderId: "197382180655",
+    appId: "1:197382180655:web:f73bba136d1595d5179bda",
+    measurementId: "G-HVDD4J7RWZ"
+    },
+  };
