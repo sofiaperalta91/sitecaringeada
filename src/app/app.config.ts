8a8b96d91c2b5b435b1ed44cf6ca162c8bd408ce
Gonçalo Cardeira <goncalo.cardeira@gmail.com>
2024-04-04 17:42:48 +0100

    build(deps): Identation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,16 +2,17 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
-import { provideFirestore, getFirestore } from "@angular/fire/firestore";
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
-    provideAnimationsAsync(), 
+    provideRouter(routes),
+    provideAnimationsAsync(),
     importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)), 
-      provideFirestore(() => getFirestore())),
-    ]
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideFirestore(() => getFirestore())
+    ),
+  ],
 };
